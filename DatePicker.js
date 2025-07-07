10b2faf237e414e8ff414daedc0f22e38d29d15a
Yasmin elkoshy <yasmin.elkoshy@gmail.com>
2025-07-07 12:41:01 +0200

    Create DatePicker.js

---
+++ DatePicker.js
@@ -0,0 +1,25 @@
+//https://demoqa.com/date-picker
+
+function SelectAssert(day,month,year){
+  document.querySelector("#datePickerMonthYearInput").click()
+
+  setTimeout(()=>{
+      // 0- 11
+      document.querySelector("select.react-datepicker__month-select").value = month
+      document.querySelector("select.react-datepicker__month-select").dispatchEvent(new Event('change',{bubbles:true}))
+      //2025
+      document.querySelector("select.react-datepicker__year-select").value = year
+      document.querySelector("select.react-datepicker__year-select").dispatchEvent(new Event('change',{bubbles:true}))
+      setTimeout(()=>{
+          document.querySelector("div.react-datepicker__day.react-datepicker__day--0"+day).click()
+          setTimeout(()=>{
+              console.log(document.querySelector("#datePickerMonthYearInput").value)
+              let result = document.querySelector("#datePickerMonthYearInput").value;
+              let expected = month+'/'+day+'/'+year
+              console.assert(result == expected)
+          },500)        
+      },500)
+  },500)
+
+}
+
